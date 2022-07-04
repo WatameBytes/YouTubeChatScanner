d07546f038f3c4861cf24fe5e5b57a5d33ac1f4a
ArisaBonsaiTree <angelevarela@hotmail.com>
2022-07-04 13:38:25 -0700

    Windows and PhotoImage

---
+++ TkinterPlayground/windows.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk() # Instantiate an ionstance of a window
+window.geometry("500x500")
+window.title("YouTube Chat Scanner")
+
+icon = PhotoImage(file="ganyu.png") # https://pixeljoint.com/pixelart/141183.htm
+window.iconphoto(True, icon)
+window.config(background="#5CFCFF")
+
+window.mainloop() # Place Window on computer screen, listen for events
