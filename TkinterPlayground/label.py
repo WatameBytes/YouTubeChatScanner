8ed41d24b8eb582262604f0d3d9e312e18a8fe71
ArisaBonsaiTree <angelevarela@hotmail.com>
2022-07-04 14:57:50 -0700

    Added labels

---
+++ TkinterPlayground/label.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+# Label = an area widget that holds text and/or an image within a window
+
+window = Tk()
+
+photo = PhotoImage(file="ganyu.png")
+
+label = \
+    Label(
+        window,
+        text="Hello World",
+        font=("Arial", 40, 'bold'),
+        fg="#00FF00",
+        bg="black",
+        relief=RAISED,
+        bd=10,
+        padx= 20,
+        pady= 20,
+        image= photo,
+        compound="bottom"
+    )
+
+label.pack()
+#label.place(x = 0, y = 0)
+
+
+window.mainloop()
