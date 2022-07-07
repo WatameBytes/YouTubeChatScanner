dd743a516fb471ccc604e091e249dae33ed26815
ArisaBonsaiTree <angelevarela@hotmail.com>
2022-07-07 01:24:08 -0700

    Basic logic structure

--- Project/main.py
+++ Project/main.py
@@ -9,11 +9,10 @@ choiceOneBuffer = False
 choiceTwoBuffer = False
 choiceThreeBuffer = False
 
-def getChatData(test):
+def getChatData():
     global choiceOneBuffer
     choiceOneBuffer = True
     print("Getting chat data...")
-    print( "\n" + test + "\n")
     time.sleep(4)
     print("\nGot Chat Data")
 
@@ -26,18 +25,36 @@ def computeChatData():
 
 
 while(True):
-    print("1: Get Chat Data\n2: Download YouTube Video\n3: Compute Chat Data")
+    print("1: Get Chat Data\n2: Download YouTube Video\n3: Compute Chat Data\n4: Exit\n5: Current Threads")
     choice = (int(input("Please pick a number: ")))
+
     if(choice == 1):
         if(choiceOneBuffer):
             print("Already downloading chat data")
         else:
-            #test = "PASSED AN ARG"
-            #Thread1 = threading.Thread(target=getChatData, args=(test, ))
             Thread1 = threading.Thread(target=getChatData)
             Thread1.start()
 
+    elif(choice == 2):
+        print()
 
+    elif(choice == 3):
+        print()
+    elif(choice == 4):
+        if(not choiceOneBuffer and not choiceTwoBuffer and not choiceThreeBuffer):
+            print("Exiting the program")
+            break
+        else:
+            print("You have some programs running in the background!")
+
+    elif(choice == 5):
+        print("===================")
+        print("Getting Chat Data: {}".format(choiceOneBuffer))
+        print("Downloading Video: {}".format(choiceTwoBuffer))
+        print("Computing Chat Data: {}".format(choiceThreeBuffer))
+        print("================\n")
+    else:
+        print("Please type 1, 2, or 3")
 
 
 
