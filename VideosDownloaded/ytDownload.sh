a254baff903f2dffe5cf3f086bfa00ea9c8e23f2
Angel Varela <64375555+ArisaBonsaiTree@users.noreply.github.com>
2023-01-26 10:50:30 -0800

    Update ytDownload.sh

--- VideosDownloaded/ytDownload.sh
+++ VideosDownloaded/ytDownload.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+# Original Code, but downloaded AV01 videos, which Adobe doesn't accept
 #yt-dlp --embed-thumbnail -S res,ext:mp4:m4a --recode mp4 $1 -N 4 -o './'$2'.%(ext)s'
+
+
 #yt-dlp --embed-thumbnail -S res,ext:mp4:m4a --recode-video MP4 $1 -N 4 -o './'$2'.%(ext)s'
 
-# Below gave us a good video but not audio
+# Below gave us a good video BUT no audio
 # yt-dlp --ignore-config -f bestvideo+bestaudio --merge-output-format mp4 $1 -N 4 -o './'$2'.%(ext)s'
+
+# This resolved the issue and doesn't give us a AV1
 yt-dlp bestvideo+bestaudio --recode-video mp4 $1 -N 4 -o './'$2'.%(ext)s'
 
-
-
-# Potential
-#yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
 echo "DONE"
